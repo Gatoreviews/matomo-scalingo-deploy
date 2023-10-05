145bd966d4c85a2d91b86c5085a16fee273319d3
Ulys <49024284+UlysC@users.noreply.github.com>
2023-10-05 18:33:19 +0200

    Update generate.config.ini.php

--- scripts/generate.config.ini.php
+++ scripts/generate.config.ini.php
@@ -4,4 +4,9 @@ $content = file_get_contents(__DIR__.'/config.ini.php.tmpl');
 ob_start();
 eval($content);
 $content = ob_get_clean();
+var_dump(__DIR__);
 file_put_contents(__DIR__.'/config.ini.php', $content);
+file_put_contents(__DIR__.'/app/config.ini.php', $content);
+file_put_contents('/app/config.ini.php', $content);
+
+`chmod +r`;
